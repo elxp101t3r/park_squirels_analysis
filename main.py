ce1abff8f935b7b3f2b206cf5164ee5c0ebf53a8
elxp101t3r <eliotx10101@outlook.com>
2024-01-03 23:00:23 +0200

    Count how many squirels in the park based on color

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = pd.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20240103.csv')
+color_dict = {
+    'Fur Color': ['Red', 'Gray', 'Black'],
+    'Count': [
+        sum(data['Primary Fur Color'] == 'Cinnamon'),
+        sum(data['Primary Fur Color'] == 'Gray'),
+        sum(data['Primary Fur Color'] == 'Black')
+    ]
+}
+colors = pd.DataFrame(color_dict)
+colors.to_csv('colors_csv.csv')
